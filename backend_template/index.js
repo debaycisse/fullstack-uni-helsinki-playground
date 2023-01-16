6b5191e8667803d3a1eb94648dee1866aa4c6ed1
Azeez Adebayo <debaycisse@gmail.com>
2023-01-16 05:58:54 +0100

    studying

--- backend_template/index.js
+++ backend_template/index.js
@@ -1,3 +1,52 @@
+// expressJS server setup
+const { response } = require('express')
+const express = require('express')
+
+const app = express()
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello, World - from ExpressJS</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
+
+
+
+
+/*
+// http server setup
 const http = require('http')
 
 let notes = [
@@ -31,3 +80,4 @@ const PORT = 3001
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
 // console.log('Hello, World!')
+*/
